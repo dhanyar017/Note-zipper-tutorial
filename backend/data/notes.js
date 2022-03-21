a84d9385faa453d9222965fafa5332c83d4f073c
dhanyar017 <dhanyaracharya017@gmail.com>
2022-03-21 07:51:23 +0530

    initialized our backend and frontend

---
+++ backend/data/notes.js
@@ -0,0 +1,19 @@
+const notes = [
+    {
+        id: 1,
+        name: "john",
+        job: "teacher"
+    },
+    {
+        id: 2,
+        name: "Robert",
+        job: "engineer"
+    },
+    {
+        id: 3,
+        name: "steve",
+        job: "doctor"
+    }
+];
+
+module.exports = notes
